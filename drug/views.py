cf055d59ec1bb785b81d2d070f4fd72c8348ecbe
jinki <jinkiller69@gmail.com>
2026-01-12 09:11:32 +0530

    pagination added

--- drug/views.py
+++ drug/views.py
@@ -5,13 +5,17 @@ from django.http import Http404, HttpResponseRedirect
 from .forms import VaccineForm
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 
 
 class VaccineList(View):
     def get(self, request):
-        vaccine_list = Drug.objects.all()
+        vaccine_list = Drug.objects.all().order_by("name")
+        paginator = Paginator(vaccine_list, 1)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
         context = {
-            "object_list": vaccine_list
+            "page_obj": page_obj
         }
         return render(request, "drug/drug-list.html", context)
 
